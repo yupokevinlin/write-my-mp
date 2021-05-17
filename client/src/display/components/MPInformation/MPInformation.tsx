0a188d6a55f92ab22a595925fc0aa1dcc0c7bff1
Kevin <yupolin921021@gmail.com>
2021-05-17 02:12:05 -0700

    MP Information update.

--- client/src/display/components/MPInformation/MPInformation.tsx
+++ client/src/display/components/MPInformation/MPInformation.tsx
@@ -23,25 +23,6 @@ export interface MPInformationEventProps {
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      display: "flex",
-      flexDirection: "row",
-      alignItems: "flex-start",
-      justifyContent: "flex-start",
-      [theme.breakpoints.up("xs")]: {
-
-      },
-      [theme.breakpoints.up("sm")]: {
-
-      },
-      [theme.breakpoints.up("md")]: {
-
-      },
-      [theme.breakpoints.up("lg")]: {
-        height: "300px",
-        width: "100%",
-      },
-    },
     paper: {
       display: "flex",
       flexDirection: "row",
@@ -252,7 +233,6 @@ const MPInformation: React.FC<MPInformationProps> = (props) => {
     currentMapPolygon,
   } = props;
 
-  console.log(currentMapPolygon);
   if (!!currentMapPolygon && !!currentMapPolygon.mpData) {
     const isVacant: boolean = currentMapPolygon.mpData.party === "Vacant";
     const name: string = isVacant ? "Vacant Seat" : `${!!currentMapPolygon.mpData.title ? `The ${currentMapPolygon.mpData.title.replace("Hon.", "Honourable")} ` : ""}${currentMapPolygon.mpData.firstName} ${currentMapPolygon.mpData.lastName}`;
@@ -262,85 +242,81 @@ const MPInformation: React.FC<MPInformationProps> = (props) => {
     const preferredLanguage: string = isVacant ? "N/A" : currentMapPolygon.mpData.contact.preferredLanguage;
 
     return (
-      <div className={classes.root}>
-        <Paper className={classes.paper} elevation={3} square>
-          {
-            isVacant ? (
-              <div className={classes.vacantPicture}>
-                <PersonIcon className={classes.vacantPictureIcon}/>
-              </div>
-            ) : (
-              <img className={classes.picture} src={currentMapPolygon.mpData.photoSrc}/>
-            )
-          }
-          <div className={classes.infoWrapper}>
-            <div className={classes.title}>
-              <Typography className={classes.titleText}>
-                {
-                  name
-                }
-              </Typography>
+      <Paper className={classes.paper} elevation={3} square>
+        {
+          isVacant ? (
+            <div className={classes.vacantPicture}>
+              <PersonIcon className={classes.vacantPictureIcon}/>
             </div>
-            <div className={classes.label}>
-              <Typography className={classes.labelText}>
-                Political Affiliation:
-              </Typography>
-            </div>
-            <div className={classes.value}>
-              <Typography className={classes.valueText}>
-                {
-                  party
-                }
-              </Typography>
-            </div>
-            <div className={classes.partyBar} style={{backgroundColor: getPartyColor(currentMapPolygon.mpData.party)}}/>
-            <div className={classes.label}>
-              <Typography className={classes.labelText}>
-                Constituency:
-              </Typography>
-            </div>
-            <div className={classes.value}>
-              <Typography className={classes.valueText}>
-                {
-                  constituencyName
-                }
-              </Typography>
-            </div>
-            <div className={classes.label}>
-              <Typography className={classes.labelText}>
-                Province / Territory:
-              </Typography>
-            </div>
-            <div className={classes.value}>
-              <Typography className={classes.valueText}>
-                {
-                  province
-                }
-              </Typography>
-            </div>
-            <div className={classes.label}>
-              <Typography className={classes.labelText}>
-                Preferred Language:
-              </Typography>
-            </div>
-            <div className={classes.value}>
-              <Typography className={classes.valueText}>
-                {
-                  preferredLanguage
-                }
-              </Typography>
-            </div>
+          ) : (
+            <img className={classes.picture} src={currentMapPolygon.mpData.photoSrc}/>
+          )
+        }
+        <div className={classes.infoWrapper}>
+          <div className={classes.title}>
+            <Typography className={classes.titleText}>
+              {
+                name
+              }
+            </Typography>
           </div>
-        </Paper>
-      </div>
+          <div className={classes.label}>
+            <Typography className={classes.labelText}>
+              Political Affiliation:
+            </Typography>
+          </div>
+          <div className={classes.value}>
+            <Typography className={classes.valueText}>
+              {
+                party
+              }
+            </Typography>
+          </div>
+          <div className={classes.partyBar} style={{backgroundColor: getPartyColor(currentMapPolygon.mpData.party)}}/>
+          <div className={classes.label}>
+            <Typography className={classes.labelText}>
+              Constituency:
+            </Typography>
+          </div>
+          <div className={classes.value}>
+            <Typography className={classes.valueText}>
+              {
+                constituencyName
+              }
+            </Typography>
+          </div>
+          <div className={classes.label}>
+            <Typography className={classes.labelText}>
+              Province / Territory:
+            </Typography>
+          </div>
+          <div className={classes.value}>
+            <Typography className={classes.valueText}>
+              {
+                province
+              }
+            </Typography>
+          </div>
+          <div className={classes.label}>
+            <Typography className={classes.labelText}>
+              Preferred Language:
+            </Typography>
+          </div>
+          <div className={classes.value}>
+            <Typography className={classes.valueText}>
+              {
+                preferredLanguage
+              }
+            </Typography>
+          </div>
+        </div>
+      </Paper>
     );
   } else {
     return (
-      <div className={classes.root}>
-        {
-          "null"
-        }
-      </div>
+      <Paper className={classes.paper} elevation={3} square>
+
+      </Paper>
     );
   }
 

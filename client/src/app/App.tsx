35a3a0018877bb222f8d07c0d5d7da5139afca46
Kevin <yupolin921021@gmail.com>
2021-05-19 00:57:14 -0700

    Added MUI Theme.

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -3,7 +3,9 @@ import {createStyles, Theme, useTheme} from "@material-ui/core";
 import {Provider} from "react-redux";
 import {configureStore} from "../state/store";
 import {makeStyles} from "@material-ui/core/styles";
-import {MapApi} from "../api/MapApi/MapApi";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import responsiveFontSizes from "@material-ui/core/styles/responsiveFontSizes";
+import {ThemeProvider} from "@material-ui/styles";
 import HomePageContainer from "../display/containers/HomePageContainer/HomePageContainer";
 
 export type AppProps = AppDataProps & AppStyleProps & AppEventProps;
@@ -36,8 +38,22 @@ export namespace AppStore {
   export const store = configureStore();
 }
 
+const getInitialMaterialUITheme = (): Theme => {
+  let theme = createMuiTheme({
+    palette: {
+      secondary: {
+        light: "#de464c",
+        main: "#d61820",
+        dark: "#951016",
+        contrastText: "#fff",
+      }
+    },
+  });
+  theme = responsiveFontSizes(theme);
+  return theme;
+};
+
 const App: React.FC<AppProps> = (props) => {
-  const theme: Theme = useTheme();
   const classes = useStyles();
 
   const {
@@ -45,11 +61,13 @@ const App: React.FC<AppProps> = (props) => {
   } = props;
 
   return (
-    <Provider store={AppStore.store}>
-      <div className={classes.app}>
-        <HomePageContainer/>
-      </div>
-    </Provider>
+    <ThemeProvider theme={getInitialMaterialUITheme()}>
+      <Provider store={AppStore.store}>
+        <div className={classes.app}>
+          <HomePageContainer/>
+        </div>
+      </Provider>
+    </ThemeProvider>
   );
 };
 
